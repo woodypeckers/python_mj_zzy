cc43b8a4f27e02464639ec2eb6c8b7c1f44b92f1
张占勇 <273829271@qq.com>
2017-06-26 23:55:27 +0800

    这个有报错，未解决

---
+++ learns/class_inherit.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python()
+# -*- coding: utf-8 -*-
+class Student(object):
+
+    def __int__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def detail(self):
+        print "A  student name is %s,age is %s" % (self.name, self.age)
+
+
+class MiddleStundent(Student):
+    def lol(self):
+        print "A  MiddleStundent name is %s,age is %s" % (self.name,self.age)
+
+class PrimaryStudent(Student):
+
+    def __init__(self, name, age, playing):
+        self.name = name
+        self.age = age
+        self.playing = playing
+
+    def play(self):
+        print "A primary student name is %s , age is %s ,playing is %s" % \
+              (self.name, self.age, self.playing)
+
+
+if __name__ == "__main__":
+
+    obj2 = PrimaryStudent('小王', 16, "youxi")
+    obj2.play()
+    obj2.detail()
+
+    obj1 = MiddleStundent('aaa', 22)
+    obj1.lol()
+    obj1.detail()
